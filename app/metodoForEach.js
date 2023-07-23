3e16604e30e3b6d3add4810c5f127d287add370e
Leonardo Ramos <leonardosr793@gmail.com>
2023-07-23 19:03:37 -0300

    completo.

---
+++ app/metodoForEach.js
@@ -0,0 +1,29 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoValorTotal = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirOsLivrosNaTela(ListaDeLivros) {
+  elementoValorTotal.innerHTML=''
+    elementoParaInserirLivros.innerHTML = ''
+    ListaDeLivros.forEach(livro => {
+      // let disponibilidade = verificaDisponibilidade(livro)
+      let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+        elementoParaInserirLivros.innerHTML += `<div class="livro">
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+         ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>`
+    });
+}
+
+// function verificaDisponibilidade(livro){
+//   if (livro.quantidade > 0){
+//     return 'livro__imagens'
+//   } else
+//    return 'livro__imagens indisponivel'
+// }
